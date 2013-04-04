03b8a2a851279e2597158466863e795ee13421eb
danielcasanovas <daniel@DanielPortatil.(none)>
2013-04-04 18:53:51 +0200

    Pujada de templates 2n intent

--- teacherAvaluation/views.py
+++ teacherAvaluation/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
+def mainpage(request):
+	template = get_template('mainpage.html')
+	variables = Context({
+		'titlehead': 'Teacheravaluation aPP',
+		'pagetitle': 'Welcome to the Teacheravaluation aPPlication',
+		'contentbody': 'OLA KE ASE'
+
+		})
+	output = template.render(variables)
+	return HttpResponse(output)
+
