95caaf6953c78bc9d3e3855fcef5603bcb4a5d34
danielcasanovas <daniel@DanielPortatil.(none)>
2013-04-07 20:48:00 +0200

    Admin modifications

--- practica1/settings.py
+++ practica1/settings.py
@@ -63,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join('/home/daniel/Django-1.5.1/Practica1', 'static')
+STATIC_ROOT = ('/home/daniel/Django-1.5.1/Practica1')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
--- teacherAvaluation/admin.py
+++ teacherAvaluation/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from teacherAvaluation.models import Teacher
+from teacherAvaluation.models import *
 
+admin.site.register(Degree)
+admin.site.register(Subject)
 admin.site.register(Teacher)
+admin.site.register(Evaluation)
+
