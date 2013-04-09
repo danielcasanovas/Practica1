98bee29de5d0fe839b54142a7f68647751bfb2e1
danielcasanovas <daniel@DanielPortatil.(none)>
2013-04-09 13:58:17 +0200

    Changes on View

--- teacherAvaluation/views.py
+++ teacherAvaluation/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import Context
 from django.template.loader import get_template
+from django.contrib.auth import authenticate, login, logout
 from teacherAvaluation.models import *
 
 def mainpage(request):
@@ -21,6 +22,52 @@ def teacher(request):
 		'pagetitle': 'Evaluacio de docencia',
 		'contentbody': 'Llistat de teachers:',
 		'teachers_list' : listOfTeachers
+		})
+	output = template.render(variables)
+	return HttpResponse(output)
+
+def degree(request):
+	template = get_template('degrees.html')
+	listOfDegrees = Degree.objects.all()
+	variables = Context({
+		'titlehead': 'Teacher Avaluation',
+		'pagetitle': 'Evaluacio de docencia',
+		'contentbody': 'Llistat de Carreres:',
+		'degrees_list' : listOfDegrees
+		})
+	output = template.render(variables)
+	return HttpResponse(output)
+
+def subject(request):
+	template = get_template('subjects.html')
+	listOfSubjects = Subject.objects.all()
+	variables = Context({
+		'titlehead': 'Teacher Avaluation',
+		'pagetitle': 'Evaluacio de docencia',
+		'contentbody': 'Llistat de Carreres:',
+		'subjects_list' : listOfSubjects
+		})
+	output = template.render(variables)
+	return HttpResponse(output)
+
+def evaluation(request):
+	template = get_template('evaluation.html')
+	listOfEvaluation = evaluation.objects.all()
+	variables = Context({
+		'titlehead': 'Teacher Avaluation',
+		'pagetitle': 'Evaluacio de docencia',
+		'contentbody': 'Llistat de Carreres:',
+		'evaluation_list' : listOfEvaluation
+		})
+	output = template.render(variables)
+	return HttpResponse(output)
+
+def logoutPage(request):
+	logout(request)
+	template = get_template('mainpage.html')
+	variables = Context({
+		'titlehead': 'Log out',
+		'contentbody': 'Has tancat la sessio.',
 		})
 	output = template.render(variables)
 	return HttpResponse(output)
